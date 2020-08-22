bad13e892acfcb1a2e3aa8f2faf3e1e1c54346dd
Juan David <gottygarcia@gmail.com>
2020-08-22 00:56:12 -0500

    configuracion de passport

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 
 const { config } = require('./config/index')
 const moviesApi = require('./routes/movies')
+const userMoviesApi = require('./routes/userMovies')
 
 const {
 	logErrors,
@@ -17,6 +18,8 @@ app.use(express.json())
 
 //routes
 moviesApi(app)
+userMoviesApi(app)
+
 //Catch 404
 app.use(notFoundHandler)
 
