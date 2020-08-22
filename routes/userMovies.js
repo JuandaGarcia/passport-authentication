a0cf48c41131d8a14e7e6d3f1b8e789c56a29fd2
Juan David <gottygarcia@gmail.com>
2020-08-22 17:23:33 -0500

    auth con passport y rutas protegidas

--- routes/userMovies.js
+++ routes/userMovies.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('passport')
 
 const UserMoviesService = require('../services/userMovies')
 const validationHandler = require('../utils/middleware/validationHandler')
@@ -7,14 +8,18 @@ const { movieIdSchema } = require('../utils/schemas/movies')
 const { userIdSchema } = require('../utils/schemas/users')
 const { createUserMovieSchema } = require('../utils/schemas/userMovies')
 
+// JWT stratgy
+require('../utils/auth/strategies/jwt')
+
 function userMoviesApi(app) {
 	const router = express.Router()
-	app.user('/api/user-movies', router)
+	app.use('/api/user-movies', router)
 
 	const userMoviesService = new UserMoviesService()
 
 	router.get(
 		'/',
+		passport.authenticate('jwt', { session: false }),
 		validationHandler({ userId: userIdSchema }, 'query'),
 		async function (req, res, next) {
 			const { userId } = req.query
@@ -32,29 +37,31 @@ function userMoviesApi(app) {
 		}
 	)
 
-	router.post('/', validationHandler(createUserMovieSchema), async function (
-		req,
-		res,
-		next
-	) {
-		const { body: userMovie } = req
+	router.post(
+		'/',
+		passport.authenticate('jwt', { session: false }),
+		validationHandler(createUserMovieSchema),
+		async function (req, res, next) {
+			const { body: userMovie } = req
 
-		try {
-			const createdUserMovieId = await userMoviesService.createUserMovie({
-				userMovie,
-			})
+			try {
+				const createdUserMovieId = await userMoviesService.createUserMovie({
+					userMovie,
+				})
 
-			res.status(201).json({
-				data: createdUserMovieId,
-				message: 'user movie created',
-			})
-		} catch (err) {
-			next(err)
+				res.status(201).json({
+					data: createdUserMovieId,
+					message: 'user movie created',
+				})
+			} catch (err) {
+				next(err)
+			}
 		}
-	})
+	)
 
 	router.delete(
 		'/:userMovieId',
+		passport.authenticate('jwt', { session: false }),
 		validationHandler({ userMovieId: movieIdSchema }, 'params'),
 		async function (req, res, next) {
 			const { userMovieId } = req.params
